f28cfea4bb5dfe044b030aa2b793a800ae353c7c
alishabelle <alishabelle98@gmail.com>
2019-08-20 02:02:47 +0000

    fixed pcs errors

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,11 +4,12 @@
 from fabric.api import *
 import datetime
 
+
 def do_pack():
     """ final archive """
-    
+
     local("mkdir -p versions")
-    date = datetime.datetime.now().strftime("%Y %b %d %H %M %S")
+    date = datetime.datetime.now().strftime("%Y%b%d%H%M%S")
     create = ('versions/web_static_' + 'date' + '.tgz')
     gen = local("tar -cvzf {} web_static".format(create))
 
