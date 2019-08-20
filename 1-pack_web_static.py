076a71576672dde1db39a3805ce408aa77be3f9b
alishabelle <alishabelle98@gmail.com>
2019-08-20 01:48:19 +0000

    push answer file fixed import

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ fabric script generates .tgz file """
 
-from fabric.api import *
+import fabric.api
 import datetime
 
 def do_pack():
     """ final archive """
     local("mkdir -p versions")
-    date = datetime.datetime.strftime("%Y %b %d %H %M %S")
+    date = datetime.datetime.now().strftime("%Y %b %d %H %M %S")
     create = ('versions/web_static_' + 'date' + '.tgz')
     gen = fabric.api.local("tar -cvzf {} web_static".format(create))
 
