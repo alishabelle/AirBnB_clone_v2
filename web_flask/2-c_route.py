3087f8c5c9756e170bcf84b3bc3c8c2e454c80dd
alishabelle <alishabelle98@gmail.com>
2019-09-04 13:26:59 +0000

    push answer file for 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Starts a Flask Web application """
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ returns hello HBNB """
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ returns HBNB """
+    return("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def txt(text):
+    """ returns C with text variable value """
+    text = text.replace('_', ' ')
+    return 'C %s' % (text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
