f828789c40a9dc5c9134458406d541ddf6fc7b0c
alishabelle <alishabelle98@gmail.com>
2019-09-04 16:30:25 +0000

    push all files for answer 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+""" Starts a Flask Web application """
+from flask import render_template
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ returns hello HBNB """
+    return("Hello HBNB!")
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ returns HBNB """
+    return("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def txt(text):
+    """ returns C with text variable value """
+    text = text.replace('_', ' ')
+    return('C %s' % (text))
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pyth(text="is cool"):
+    """ returns Python with text variable value """
+    text = text.replace('_', ' ')
+    return('Python %s' % (text))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def num(n):
+    """ displays only if n is an int """
+    return('%d is a number' % (n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def temp(n=None):
+    """ makes html template """
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
